55abe3b4f19728b0b0ef290e417e2464c3b18134
JonathanPlanells <jonathanplanells@hotmail.com>
2023-01-03 23:36:17 -0500

    Se crear el contenido editable del DOM con ´, ya se puede agregar el elemento a la lista, siguiente paso que se agreguen mas elementos y no solo cambie el primero

--- script.js
+++ script.js
@@ -37,8 +37,16 @@ const creatTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value
+    const task = document.querySelector("[data-task]")
     input.value = ""
-    console.log(value)
+    const content = ` 
+    <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    console.log(content)
 }
 
 btn.addEventListener('click', creatTask)
